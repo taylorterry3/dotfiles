3b0b346f923c4acf01f201c78d778fc78fe51cd7
Taylor Terry <User@T430u-TTerry.(none)>
2014-09-15 20:51:25 -0400

    symlinks for recently added files

--- make_links.sh
+++ make_links.sh
@@ -5,4 +5,6 @@
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.bashrc_private ~/.bashrc_private
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.Rprofile ~/.Rprofile
 
