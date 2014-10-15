412325be887573e235013732d0bef1b74eabfca6
Taylor Terry <taylor.terry@gmail.com>
2014-10-14 22:24:46 -0400

    add .curlrc and update symlinks

--- make_links.sh
+++ make_links.sh
@@ -7,4 +7,5 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.bashrc_private ~/.bashrc_private
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.Rprofile ~/.Rprofile
+ln -s ~/dotfiles/.curlrc ~/.curlrc
 
