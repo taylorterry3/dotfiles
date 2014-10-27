20c9357dd75e05b23b0256eaf9a0d06724b6521f
taylorterry3 <taylor.terry@gmail.com>
2014-10-27 18:55:47 -0400

    comment out .bashrc_private symlink

--- make_links.sh
+++ make_links.sh
@@ -4,7 +4,7 @@
 
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.bashrc_private ~/.bashrc_private
+# ln -s ~/dotfiles/.bashrc_private ~/.bashrc_private
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.Rprofile ~/.Rprofile
 ln -s ~/dotfiles/.curlrc ~/.curlrc
