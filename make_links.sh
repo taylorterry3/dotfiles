6c236fab0af561631769fbcd54e7799773121af2
Taylor Terry <taylor.terry@gmail.com>
2015-12-08 10:39:29 -0500

    I didn't have a screenrc? werid.

--- make_links.sh
+++ make_links.sh
@@ -3,6 +3,7 @@
 # makes symlinks in home dir to dotfiles dir.
 
 ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.screenrc ~/.screenrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 # ln -s ~/dotfiles/.bashrc_private ~/.bashrc_private
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
