b1e606a7371b4a6624955482037e9695532e5673
Taylor <taylor.terry@gmail.com>
2019-10-24 14:09:46 -0400

    clean up symlinker

--- make_links.sh
+++ make_links.sh
@@ -5,14 +5,6 @@
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.screenrc ~/.screenrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-# ln -s ~/dotfiles/.bashrc_private ~/.bashrc_private
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.Rprofile ~/.Rprofile
-ln -s ~/dotfiles/.curlrc ~/.curlrc
-ln -s ~/Google\ Drive gdrive
-
-# check if on OSX and use .profile if so
-OS=`uname`
-if [ $OS = "Darwin" ]; then
-	ln -s ~/dotfiles/.profile ~/.profile
-fi
+ln -s ~/dotfiles/.profile ~/.profile
