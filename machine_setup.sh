2b7914d1fdf4c7d87f8b5ba478ad015863960a2c
Taylor Terry <taylor.terry@gmail.com>
2019-10-25 15:26:49 -0400

    more cleanup

--- machine_setup.sh
+++ machine_setup.sh
@@ -12,12 +12,6 @@ sudo apt install build-essential gcc golang libbz2-dev libffi-dev libreadline-de
 
 echo "Finished adding PPAs and installing applications"
 
-echo "Manually downloading some installers\nBe careful, there is hardcoding to tterry here"
-
-# not sure if I need this old vim stuff tbh
-# mkdir -p ~/.vim/autoload ~/.vim/bundle
-# chown -R ~/.vim tterry
-
 echo "Setting up pyenv"
 
 curl https://pyenv.run | bash
